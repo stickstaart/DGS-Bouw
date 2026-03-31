9796062e7312357b23fe3a14da41148b0182661c
JØƵ <jos@tharonline.nl>
2026-03-31 11:21:02 +0200

    Deleted Unused Code
    projects.ts en under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import Projects from '@/components/Projects';
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
-import UnderConstruction from '@/components/UnderConstruction';
 
 export default function Home() {
   return (
     <main>
       {/* Hier komen straks je Hero en Navbar */}
       <Hero />
-{/*      <UnderConstruction/>*/}
       <Projects />
       <About />
       <Contact />
